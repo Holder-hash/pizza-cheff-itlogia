5b6b2f5ef0814eac646fdb2df286731bbe2e508b
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-12-10 17:22:48 +0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './feauters/header/header.component';
+import { HeroComponent } from './feauters/hero/hero.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HeroComponent],
+  imports: [BrowserModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductViewerComponent } from './components/product-viewer/product-viewer.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductViewerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
